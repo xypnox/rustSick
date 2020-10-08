02b1ef454023557f91b063db816aedae55767262
xypnox <xypnox@gmail.com>
2020-10-08 19:06:01 +0530

    Add tests and fix the function for high count

--- grokking-algo/src/binary_search.rs
+++ grokking-algo/src/binary_search.rs
@@ -7,7 +7,7 @@ pub fn binary_search(arr: Vec<i32>, item: i32) -> Option<usize> {
 
     let mut mid;
     let mut low = 0;
-    let mut high = arr.len();
+    let mut high = arr.len() - 1;
 
     while low <= high {
         mid = (low + high) / 2;
@@ -20,4 +20,13 @@ pub fn binary_search(arr: Vec<i32>, item: i32) -> Option<usize> {
     }
 
     None
+}
+
+#[test]
+fn tests() {
+    assert_eq!(binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8], 1), Some(0));
+    assert_eq!(binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8], 3), Some(2));
+    assert_eq!(binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8], 4), Some(3));
+    assert_eq!(binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8], 7), Some(6));
+    assert_eq!(binary_search(vec![1, 2, 3, 4, 5, 6, 7, 8], 10), None);
 }
